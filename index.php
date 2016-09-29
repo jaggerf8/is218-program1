328d2d9f0931d4b5d4d04fe0b523e37e528afc97
Jagger S Ferreira <jsf22@afsconnect1.njit.edu>
2016-09-29 13:36:38 -0400

    basic php stuff

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-
+// start php
      echo 'hello world';
+    // print text php
+     $myvar = 'Some text i wanna store in a variable';
+    // store text in a avriable
+     echo '<br>';
+    // line break in html
+     echo '$myvar';
+     echo '<br>';
+     echo "$myvar";
+     // quotes make u put what is in the vairable
 ?>
