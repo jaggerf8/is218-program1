5741b286b42717becb95d6831acef7fc0134b018
Jagger S Ferreira <jsf22@afsconnect1.njit.edu>
2016-09-29 13:58:11 -0400

    first php file

--- index.php
+++ index.php
@@ -9,5 +9,37 @@
      echo '$myvar';
      echo '<br>';
      echo "$myvar";
-     // quotes make u put what is in the vairable
+     // quotes make u put what is in the variable
+
+     echo '<br>';
+
+     //example of array
+     $myarray = array();
+     $myarray[] = 'some value 1';
+     $myarray[] = 'some value 2';
+     $myarray[] = 'some value 3';
+
+     print_r($myarray);
+     // print_r is used to see inside the array etc
+
+    echo '<br>';
+
+     $myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+    // example of an asscoiated array and nested array
+     print_r($myAssoc);
+
+    print_r($myAssoc['value1']);
+    // print value key
+
+
+
+
+  //making a class
+   class myclass
+   {
+   }    
+  //this is how u instantiate an object
+  $obj = new myclass;
+  print_r($obj);
+
 ?>
